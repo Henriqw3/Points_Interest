dc70696e2fd67018bcb6edd99630763f6795570b
Henriqw3 <henrique.knows42@gmail.com>
2019-12-19 20:46:29 -0300

    creating controllers

---
+++ src/main/kotlin/br/com/xyinc/deviceplatform/controllers/PointsController.kt
@@ -0,0 +1,45 @@
+package br.com.xyinc.deviceplatform.controllers
+
+import br.com.xyinc.deviceplatform.documents.PointOfInterest
+import br.com.xyinc.deviceplatform.dtos.PointOfInterestDto
+import br.com.xyinc.deviceplatform.response.Response
+import br.com.xyinc.deviceplatform.services.POIsService
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindingResult
+import org.springframework.validation.ObjectError
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
+
+
+@RestController
+@RequestMapping("/pointofinterest")
+class PointsController (val poisService: POIsService){
+
+    @PostMapping
+    fun registerPoint(@Valid @RequestBody pointDto: PointOfInterestDto,
+                      result: BindingResult): ResponseEntity<Response<PointOfInterestDto>> {
+
+        val response: Response<PointOfInterestDto> = Response<PointOfInterestDto>()
+        validatePoint(pointDto, result)
+
+        if(result.hasErrors()) {
+            for(e in result.allErrors) response.erros.add(e.defaultMessage!!)
+            return ResponseEntity.badRequest().body(response)
+        }
+        return ResponseEntity.ok(response)
+    }
+
+    private fun validatePoint(poiDto: PointOfInterestDto, result: BindingResult) {
+
+        if(poiDto.id == null){
+            result.addError(ObjectError("pointOfInterest",
+                    "The point must have some ID"))
+            return
+        }
+        val point: PointOfInterest? = poisService.getPointByCoordinate(poiDto.poi_x, poiDto.poi_y)
+        if(point == null) {
+            result.addError(ObjectError("pointOfInterest",
+                    "The point not found"));
+        }
+    }
+}
